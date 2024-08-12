c185df6e4a488415eaafd767f88008cf66a35afa
Sajal Namdeo <sajalnamdeo627@gmail.com>
2024-08-12 17:28:09 +0530

    [feat]: Add Marketplace section

--- frontend/src/pages/Services/components/marketplace/Marketplace.jsx
+++ frontend/src/pages/Services/components/marketplace/Marketplace.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Cards from './components/cards/Cards'
 
 const Marketplace = () => {
   return (
     <>
     <h1 className='text-center font-semibold text-3xl'>MarketPlace</h1>
+    <Cards/>
     </>
   )
 }
--- frontend/src/pages/Services/components/marketplace/components/card/Card.jsx
+++ frontend/src/pages/Services/components/marketplace/components/card/Card.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <>
+    <div className='bg-white'>
+      <img src="/home_image.png" alt="" />
+      <h2>Card Title</h2>
+    </div>
+    </>
   )
 }
 
